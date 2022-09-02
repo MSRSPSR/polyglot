2c1c5917450048b65d9495e3cf91b2f0828c6e6c
Muhammed Abiola <hi@mabiola.net>
2022-09-02 20:16:30 +0100

    Cleanup

--- rs/target/debug/deps/playground-9291cb57d95a5c40.d
+++ rs/target/debug/deps/playground-9291cb57d95a5c40.d
@@ -1,5 +1,5 @@
-/Users/mabiola/src/playground/target/debug/deps/playground-9291cb57d95a5c40.rmeta: src/main.rs
+/Users/mabiola/src/polyglot/rs/target/debug/deps/playground-9291cb57d95a5c40.rmeta: src/main.rs
 
-/Users/mabiola/src/playground/target/debug/deps/playground-9291cb57d95a5c40.d: src/main.rs
+/Users/mabiola/src/polyglot/rs/target/debug/deps/playground-9291cb57d95a5c40.d: src/main.rs
 
 src/main.rs:
