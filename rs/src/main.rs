ba0f09d870b74faaea9720d5503a11b0109159fd
Muhammed Abiola <hi@mabiola.net>
2022-09-02 21:38:22 +0100

    Enums

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,12 +1,49 @@
-fn main() {
-    let a = vec![];
-    let mut b = a;
 
+// Functional types with enums
+enum RSEnum {
+    Foo(i32),
+    Bar(String),
+    Baz(Vec<String>),
+    Faz(Option<i32>),
+    Far(fn() -> i32);
+}
+
+fn far() -> i32 {
+    return 5;
+}
+
+fn main() {
+    let a: Vec<i32> = vec![];
+    let mut b = a;
+    
     b.push(1);
     println!("{:?}", b);
 
-    let x = 5;
-    let y = &x;
+    // Shadowing
+    let a = "Truth";
 
-    println!("{:?}", x + y);
+    let mut x = 5;
+    let y = &x;
+    {
+        let z = &mut x;
+
+        *z = 7;
+        println!("{:?}", z);
+    }
+
+    x = 9;
+    println!("{:?}", x);
+
+    let foo = RSEnum::Foo(bar);
+
+    // Pattern matching according to type
+    if let RSEnum::Foo(value) = foo { }
+    match foo {
+        RSEnum::Foo(value) => {
+
+        },
+        RSEnum::Faz(Some(value)) => { },
+        RSEnum::Faz(None) => { },
+        _ => {}
+    }
 }
--- ts/test.ts
+++ ts/test.ts
@@ -1,5 +1,15 @@
+type Foo = () => void;
+
 const a = [];
 const b = a;
 
 b.push(1);
 console.log(a);
+
+// Enums
+
+enum TSEnum {
+  Foo,
+  Bar,
+  Baz
+}
